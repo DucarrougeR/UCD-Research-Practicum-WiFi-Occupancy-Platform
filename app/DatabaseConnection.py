168cc1d4c04fa0af9ed116a3aa470d40863ce3f8
Romain <romainducarrouge@gmail.com>
2016-06-27 12:38:03 +0100

    addition

--- app/DatabaseConnection.py
+++ app/DatabaseConnection.py
@@ -1,15 +1,40 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+
+
+
+
+
+
+
+
 # CONNECTION
 conn = sqlite3.connect("TimeTable.db") # Connect to database (creates if it does not exist)
 cursor = conn.cursor()
 
 # Create a new table in the current database
 # Specify column names and data types
-cursor.execute("CREATE TABLE IF NOT EXISTS TimeTable ( ... )")
-               # replace ... with
-               # the Name for all columns of data to add to tables as follows
-               # ClassRoom text, Occupancy integer, Full boolean, Empty boolean...
-
-
+cursor.execute("CREATE TABLE IF NOT EXISTS TimeTable (class_room  String(10), time DateTime, date Integer, week_day Text, month Text, year Integer, associated Integer, authenticated Integer, full Boolean)")
 conn.commit() # Save the changes
 
 # INSERTION
