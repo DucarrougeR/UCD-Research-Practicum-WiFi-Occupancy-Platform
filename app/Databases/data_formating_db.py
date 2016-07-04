510d2543edb4d12120e88bd9e3640a4307e1c84a
Romain <romainducarrouge@gmail.com>
2016-07-04 15:05:01 +0100

    updates

--- app/Databases/data_formating_db.py
+++ app/Databases/data_formating_db.py
@@ -18,23 +18,23 @@ df = df.drop(df.columns[[0]], axis=1)
 dataframes = [New1, df]
 result = pd.concat(dataframes, axis=1)
 
-# Working on splitting the time fields for database
-time = result['time']
-t2 = result['time'].str.partition(' ')
-t2.columns = ['day', 'space', 'rest']
-t3 = t2['rest'].str.partition(' ')
-t3.columns = ['month', 'space', 'rest']
-t4 = t3['rest'].str.partition(' ')
-t4.columns = ['date', 'space', 'rest']
-t5 = t4['rest'].str.partition(' ')
-t5.columns = ['time', 'space', 'rest']
-t6 = t5['rest'].str.partition(' ')
-t6.columns = ['gmt', 'space', 'year']
-
-dfTime = [t2['day'], t3['month'], t4['date'], t5['time'], t6['year']]
-newTime = pd.concat(dfTime, axis=1)
-Data = [result['campus'],result['building'],result['room'], newTime, result['assocated'], result['authenticated']]
-dataFinal = pd.concat(Data, axis=1)
+# # Working on splitting the time fields for database
+# time = result['time']
+# t2 = result['time'].str.partition(' ')
+# t2.columns = ['day', 'space', 'rest']
+# t3 = t2['rest'].str.partition(' ')
+# t3.columns = ['month', 'space', 'rest']
+# t4 = t3['rest'].str.partition(' ')
+# t4.columns = ['date', 'space', 'rest']
+# t5 = t4['rest'].str.partition(' ')
+# t5.columns = ['time', 'space', 'rest']
+# t6 = t5['rest'].str.partition(' ')
+# t6.columns = ['gmt', 'space', 'year']
+#
+# dfTime = [t2['day'], t3['month'], t4['date'], t5['time'], t6['year']]
+# newTime = pd.concat(dfTime, axis=1)
+# Data = [result['campus'],result['building'],result['room'], newTime, result['assocated'], result['authenticated']]
+# dataFinal = pd.concat(Data, axis=1)
 
 #Renaming column to fix header
 dataFinal = dataFinal.rename(columns = {'assocated':'associated'})
@@ -44,21 +44,21 @@ dataFinal = dataFinal.rename(columns = {'assocated':'associated'})
 # FORMATTING GROUND TRUTH FILE
 df2 = pd.read_csv("gt_clean.csv")
 
-t2 = df2['time'].str.partition(' ')
-t2.columns = ['day', 'space', 'rest']
-t3 = t2['rest'].str.partition(' ')
-t3.columns = ['month', 'space', 'rest']
-t4 = t3['rest'].str.partition(' ')
-t4.columns = ['date', 'space', 'rest']
-t5 = t4['rest'].str.partition(' ')
-t5.columns = ['time', 'space', 'year']
-
-dfTime = [t2['day'], t3['month'], t4['date'], t5['time']]
-newTime = pd.concat(dfTime, axis=1)
-# newTime.head()
-
-frame = [df2['room'],df2['capacity'], df2['occupancy'], newTime]
-finalFrame = pd.concat(frame, axis=1)
+# t2 = df2['time'].str.partition(' ')
+# t2.columns = ['day', 'space', 'rest']
+# t3 = t2['rest'].str.partition(' ')
+# t3.columns = ['month', 'space', 'rest']
+# t4 = t3['rest'].str.partition(' ')
+# t4.columns = ['date', 'space', 'rest']
+# t5 = t4['rest'].str.partition(' ')
+# t5.columns = ['time', 'space', 'year']
+#
+# dfTime = [t2['day'], t3['month'], t4['date'], t5['time']]
+# newTime = pd.concat(dfTime, axis=1)
+# # newTime.head()
+#
+# frame = [df2['room'],df2['capacity'], df2['occupancy'], newTime]
+# finalFrame = pd.concat(frame, axis=1)
 #finalFrame.head()
 
 finalFrame.to_csv("formattedGroundTruthData.csv")
@@ -67,20 +67,20 @@ finalFrame.to_csv("formattedGroundTruthData.csv")
 # FORMATTING TIME TABLE FILE
 df3 = pd.read_csv("timetable_clean.csv")
 
-d2 = df3['time'].str.partition(' ')
-d2.columns = ['day', 'space', 'rest']
-d3 = d2['rest'].str.partition(' ')
-d3.columns = ['month', 'space', 'rest']
-d4 = d3['rest'].str.partition(' ')
-d4.columns = ['date', 'space', 'rest']
-d5 = d4['rest'].str.partition(' ')
-d5.columns = ['time', 'space', 'year']
-
-dfConcat = [d2['day'], d3['month'], d4['date'], d5['time']]
-Update = pd.concat(dfConcat, axis=1)
-
-NewFrame = [Update, df3['room'], df3['module'], df3['size']]
-Finish = pd.concat(NewFrame, axis=1)
+# d2 = df3['time'].str.partition(' ')
+# d2.columns = ['day', 'space', 'rest']
+# d3 = d2['rest'].str.partition(' ')
+# d3.columns = ['month', 'space', 'rest']
+# d4 = d3['rest'].str.partition(' ')
+# d4.columns = ['date', 'space', 'rest']
+# d5 = d4['rest'].str.partition(' ')
+# d5.columns = ['time', 'space', 'year']
+#
+# dfConcat = [d2['day'], d3['month'], d4['date'], d5['time']]
+# Update = pd.concat(dfConcat, axis=1)
+#
+# NewFrame = [Update, df3['room'], df3['module'], df3['size']]
+# Finish = pd.concat(NewFrame, axis=1)
 
 #Formatting 'None' value to NULL for future SQL queries
 Finish['module'].replace('None', "NULL", inplace=True)
--- app/Databases/db_connection.py
+++ app/Databases/db_connection.py
@@ -1,64 +1,100 @@
 #Romain Ducarrouge
+from flask import Flask
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
-from db import session
-from db import model
+from sqlalchemy import Integer, ForeignKey, String, Column
+from flask_sqlalchemy import SQLAlchemy
 
-Base = declarative_base()
 
-# create the Database
-# create the tables for Logs and for Ground Truth data
-# use Room Number as Foreign Key for 2 tables
+app = Flask(__name__)
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:////database.db'
+db = SQLAlchemy(app)
 
-class Logs(db.Model):
-    __tablename__ = 'Logs'
-    campus = db.Column(db.String(20))
-    building = db.Column(db.String(20), primary_key=True)
-    room = db.Column(db.String(10), primary_key=True, db.ForeignKey)
-    date = db.Column(db.DateTime)
-    associated = db.Column(db.Integer)
-    authenticated = db.Column(db.Integer)
+# create the Database with 3 tables
+class Class(db.Model):
+    __tablename__ = 'class'
+    module = Column(String(20), primary_key=True, nullable=False)
+    size = Column(Integer)
+    room = Column(String(6))
+    day = Column(String(4))
+    time = Column(DateTime)
+    date = Column(Integer)
+    month = Column(String)
+    lecture = Column(Boolean, default=True)
 
-    def __init__(self, campus, building, room, date, associated, authenticated):
+    def __init__(self, module, size, room, day, time, date, month):
+        self.module = module
+        self.size = size
+        self.room = room
+        self.day = day
+        self.time = time
+        self.date = date
+        self.month = month
+
+    def __repr__(self):
+        return "<Class module='%s', size='%d', room='%s'>" % (self.module, self.size, self.room)
+################################################################################
+
+class Building(db.Model):
+    __tablename__ = 'building'
+    room = db.Column(db.String(6), primary_key=True, nullable=False)
+    campus = db.Column(db.String(6))
+    building = db.Column(db.String(6))
+    capacity = db.Column(db.Integer)
+    classRoom = db.Column(db.Integer, db.ForeignKey('class.room'))
+
+    def __init__(self, room, campus, building, capacity):
+        self.room = room
         self.campus = campus
         self.building = building
+        self.capacity = capacity
+
+    def __repr__(self):
+        return "<Building room='%s', campus='%s', building='%s', capacity='%d')>" % (self.room, self.campus, self.building, self.capacity)
+################################################################################
+
+class Counts(db.Model):
+    __tablename__ = 'counts'
+    room = db.Column(db.String(6), db.ForeignKey('building.room'), nullable=False)
+    day = db.Column(db.String(6))
+    month = db.Column(db.String(6))
+    date = db.Column(db.Integer)
+    time = db.Column(db.DateTime)
+    year = db.Column(db.Integer)
+    associated = db.Column(db.Integer)
+    authenticated = db.Column(db.Integer)
+    occupancy = db.Column(db.String)
+    occupancyCount = db.Column(db.Integer)
+
+    def __init__(self, room, day, month, date, time, year, associated, authenticated, occupancy, occupancyCount):
         self.room = room
+        self.day = day
+        self.month = month
         self.date = date
+        self.time = time
+        self.year = year
         self.associated = associated
         self.authenticated = authenticated
-
-    def __repr__(self):
-        return '<User %r>' % self.room
-################################################################################
-
-class GTdata(db.Model):
-    __tablename__ = 'GroundTruthData'
-    room = db.Column(db.String(10), db.ForeignKey('Logs.room'))
-    capacity = db.Column(db.Integer)
-    time = db.Column(db.DataTime)
-    occupancy = db.Column(db.Integer)
-
-    def __init__(self, room, capacity, time, occupancy):
-        self.room = room
-        self.capacity = capacity
-        self.time = time
         self.occupancy = occupancy
+        self.occupancyCount = occupancyCount
 
     def __repr__(self):
-        return '<User %r>' % self.room
+        return "<Counts room='%s', associated='%d', authenticated='%d', occupancyCount='%d')>" % (self.room, self.associated, self.authenticated, self.occupancyCount)
 ################################################################################
 
-def setup():
-    engine = create_engine('sqlite:///./datebase.db', echo=True)
-    connection = engine.connect()
-    Base.metadata.create_all(connection, bind=engine)
 
 
-def teardown():
-    session.remove
+# def setup():
+#     engine = create_engine('sqlite:///./datebase.db', echo=True)
+#     if not database_exists(engine.url):
+#         create_database(engine.url)
+#     connection = engine.connect()
+#     Base.metadata.create_all(connection, bind=engine)
 
-def test_connection():
+
+# def teardown():
+#
+# def test_connection():
 
 '''sources
 engine creation: http://docs.sqlalchemy.org/en/latest/core/engines.html
