08c9fd7f4aa6c1b16b55228d6423f982eb30ea09
Romain <romainducarrouge@gmail.com>
2016-07-05 15:36:24 +0100

    further cleaning for merging of dataframes

--- app/mod_db/db_creation.py
+++ app/mod_db/db_creation.py
@@ -1,76 +1,18 @@
 #Romain Ducarrouge
 # from flask import Flask
-# from sqlalchemy import *
+# from sqlalchemy import 
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+def init_db():
+	db = get_db()
+	with app.open_resource("schema.sql", more='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
 
-
-app = Flask(__name__)
-#Tells SQLAlchemy the path to the database and the username and passwords
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:////tmp/app/Databases/database.db'
-db = SQLAlchemy(app)
-
-# create the Database with 3 tables
-class Class(db.Model):
-    __tablename__ = 'class'
-    module = db.Column(db.String(20), primary_key=True, nullable=False)
-    size = db.Column(db.Integer)
-    room = db.Column(db.String(6))
-    timeStamp = db.Column(db.DateTime)
-    lecture = db.Column(db.Boolean, default=True)
-    building = db.relationship('Building', backref='Class', lazy='dynamic')
-    counts = db.relationship('Counts', backref='Class', lazy='dynamic')
-
-    def __init__(self, module, size, room, timeStamp, lecture):
-        self.module = module
-        self.size = size
-        self.room = room
-        self.timeStamp = timeStamp
-        self.lecture = lecture
-
-    def __repr__(self):
-        return "<Class module='%s', size='%d', room='%s'>" % (self.module, self.size, self.room)
-################################################################################
-
-class Building(db.Model):
-    __tablename__ = 'building'
-    room = db.Column(db.String(6), primary_key=True, nullable=False, unique=True)
-    campus = db.Column(db.String(6))
-    building = db.Column(db.String(6))
-    capacity = db.Column(db.Integer)
-    classRoom = db.Column(db.Integer, db.ForeignKey('class.room'))
-
-    def __init__(self, room, campus, building, capacity):
-        self.room = room
-        self.campus = campus
-        self.building = building
-        self.capacity = capacity
-
-    def __repr__(self):
-        return "<Building room='%s', campus='%s', building='%s', capacity='%d')>" % (self.room, self.campus, self.building, self.capacity)
-################################################################################
-
-class Counts(db.Model):
-    __tablename__ = 'counts'
-    room = db.Column(db.String(6), db.ForeignKey('building.room'), nullable=False)
-    timestamp = db.Column(db.DateTime)
-    associated = db.Column(db.Integer)
-    authenticated = db.Column(db.Integer)
-    occupancy = db.Column(db.String(6))
-    occupancyCount = db.Column(db.Integer)
-
-    def __init__(self, room, timestamp, associated, authenticated, occupancy, occupancyCount):
-        self.room = room
-        self.timestamp = timestamp
-        self.associated = associated
-        self.authenticated = authenticated
-        self.occupancy = occupancy
-        self.occupancyCount = occupancyCount
-
-    def __repr__(self):
-        return "<Counts room='%s', associated='%d', authenticated='%d', occupancyCount='%d')>" % (self.room, self.associated, self.authenticated, self.occupancyCount)
-################################################################################
+@app.cli.command('initdb')
+def initdb_command():
+	# initialize database
+	init_db()
+	print ("Database initialized")
 
 
 '''sources
--- app/mod_db/theApp.py
+++ app/mod_db/theApp.py
@@ -1,82 +1,63 @@
+import os
 import sqlite3
-import json
-from flask import Flask, g, request, abort, session, redirect, render_template, flash, url_for
-
-# Create our application
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+	 
+# Create the application
 app = Flask(__name__, static_url_path='')
-app.config.from_object('config')
+app.config.from_object(__name__)
 
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'database.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('COMP47350', silent=True)
 
 def connect_db():
-    # Connection to database
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-def init_db():
-    # Creates the tables
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    # Connects to database
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 def get_db():
-    # Opens a new database connection if needed
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = connect_db()
-    return db
+    # Opens database connection 
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 
 @app.teardown_appcontext
-def close_db(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+def close_db(error):
+    # Closes the database after request
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+	
+def init_db():
+	db = get_db()
+	with app.open_resource("schema.sql", more='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+	# initialize database
+	init_db()
+	print ("Database initialized")
 
 
-@app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
-
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-
-
+	
 if __name__ == '__main__':
-    init_db()
+    #init_db()
     app.run()
+	
+	
+	
+	
+	
+'''sources
+engine creation: http://docs.sqlalchemy.org/en/latest/core/engines.html
+video for SQLAlchemy https://www.youtube.com/watch?v=f_-ApViOv20
+GitHub repo for Flaskr tutorial to flask and sql
+'''	
