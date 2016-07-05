1896646f5065377932788e8ef628e5abff5630db
Luke Kearney <luke.t.kearney@gmail.com>
2016-07-05 15:46:23 +0100

    Tidied front end directories. Added config file

--- app/static/karma.conf.js
+++ app/static/karma.conf.js
@@ -8,34 +8,21 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
-<<<<<<< HEAD
       '**/*.module.js',
       '*!(.module|.spec).js',
       '!(bower_components)/**/*!(.module|.spec).js',
       '**/*.spec.js'
-=======
-      'components/**/*.js',
-      'view*/**/*.js'
->>>>>>> 2bd9720733ff289c7c4be284115af17d674c92e5
     ],
 
     autoWatch: true,
 
     frameworks: ['jasmine'],
 
-<<<<<<< HEAD
     browsers: ['Chrome', 'Firefox'],
-=======
-    browsers: ['Chrome'],
->>>>>>> 2bd9720733ff289c7c4be284115af17d674c92e5
 
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
-<<<<<<< HEAD
-      'karma-jasmine'
-    ]
-=======
       'karma-jasmine',
       'karma-junit-reporter'
     ],
@@ -44,7 +31,6 @@ module.exports = function(config) {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
     }
->>>>>>> 2bd9720733ff289c7c4be284115af17d674c92e5
 
   });
 };
