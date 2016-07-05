5a1cea49212c57fc01f39adad68fda10d50c3310
Mark <markgyr@gmail.com>
2016-07-05 13:08:07 +0100

    Structure for tests and stats module.

---
+++ app/tests/tests_basic.py
@@ -0,0 +1,28 @@
+import unittest, sys
+
+class DataTests(unittest.TestCase):
+    """
+    Data cleaning and formatting.
+    """    
+    # Manually tests the correctness of some rows in the master log data.
+    def test_log_data_correctness(self):
+        self.assertEqual(foo, bar)
+
+    # Manually tests the correctness of some rows in the ground truth data.
+    def test_ground_truth_data_correctness(self):
+        pass
+
+    # Manually tests the correctness of some rows in the timetable data. 
+    def test_timetable_data_correctness(self):
+        pass
+
+class DataBaseTests(unittest.TestCase):
+    """
+    Database
+    """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
