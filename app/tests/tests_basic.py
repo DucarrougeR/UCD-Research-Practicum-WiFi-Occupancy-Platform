8aaa1a6ae10d513fec4359b46357d684a5a072ec
Luke Kearney <luke.t.kearney@gmail.com>
2016-07-06 10:52:12 +0100

    Cleaned test file

--- app/tests/tests_basic.py
+++ app/tests/tests_basic.py
@@ -1,23 +1,24 @@
 import unittest, sys, os, tempfile
 from app.mod_db.QueryBuilder import QueryBuilder
+from app.mod_db import theApp
 
-class DataTests(unittest.TestCase):
-    ''' Data cleaning and formatting '''
-    # Manually tests the correctness of some rows in the master log data.
-    def test_log_data_correctness(self):
-        self.assertEqual(foo, bar)
-
-    # Manually tests the correctness of some rows in the ground truth data.
-    def test_ground_truth_data_correctness(self):
-        pass
-
-    # Manually tests the correctness of some rows in the timetable data.
-    def test_timetable_data_correctness(self):
-        pass
+# class DataTests(unittest.TestCase):
+#     ''' Data cleaning and formatting '''
+#     # Manually tests the correctness of some rows in the master log data.
+#     def test_log_data_correctness(self):
+#         self.assertEqual(foo, bar)
+#
+#     # Manually tests the correctness of some rows in the ground truth data.
+#     def test_ground_truth_data_correctness(self):
+#         pass
+#
+#     # Manually tests the correctness of some rows in the timetable data.
+#     def test_timetable_data_correctness(self):
+#         pass
 
 class DataBaseTests(unittest.TestCase):
     ''' Database '''
-    def SetUp(selfself):
+    def SetUp(self):
         self.db_fd, theApp.app.config['DATABASE'] = tempfile.msktemp()
         theApp.app.config['TESTING'] = True
         self.app = theApp.app.test_client()
