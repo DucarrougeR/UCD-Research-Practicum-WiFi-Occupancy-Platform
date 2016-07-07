fe259f0f610964f4444bca62b54abb84d8a8aa1e
Luke Kearney <luke.t.kearney@gmail.com>
2016-07-07 16:23:52 +0100

    Front-end: updated datepicker. Cleaned up templates. Some restyling

--- app/static/app/occupancy.js
+++ app/static/app/occupancy.js
@@ -2,9 +2,8 @@
 
 var occupancyApp = angular.module('occupancyApp', [
   'ngRoute',
-  'datePicker',
-  'chart.js',
-  'ui.bootstrap'
+  'pikaday',
+  'chart.js'
 ]);
 
 occupancyApp.controller('primaryController', function($scope) {
@@ -25,13 +24,7 @@ occupancyApp.controller("lineCtrl", ['$scope', '$timeout', function ($scope, $ti
 
 
 
-  // Simulate async data update
-  // $timeout(function () {
-  //   $scope.data = [
-  //     [28, 48, 40, 19, 86, 27, 90, 65, 59, 80, 81, 56, 55, 40]
-  //   ];
-  // }, 3000);
 
-  //[28, 48, 40, 19, 86, 27, 90, 65, 59, 80, 81, 56, 55, 40]
-  // [65, 59, 80, 81, 56, 55, 40, 28, 48, 40, 19, 86, 27, 90]
+
+  
 }]);
