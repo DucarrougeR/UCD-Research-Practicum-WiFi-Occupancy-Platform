1813e67bae8a2da039e52de928787f0d9b3f1bf3
Romain <romainducarrouge@gmail.com>
2016-07-11 13:23:39 +0100

    Primary and Foreign keys structure for tables

--- app/mod_db/schema.sql
+++ app/mod_db/schema.sql
@@ -1,26 +1,31 @@
 drop table if exists classes;
-create table classes (
-  classes_module_code string primary key not null,
-  classes_size integer primary key not null,
+CREATE TABLE classes (
+  classes_module_code string not null,
+  classes_size integer not null,
   classes_room_number string not null,
-  classes_time datetime
+  classes_time datetime,
+  PRIMARY KEY (classes_module_code, classes_time),
+  FOREIGN KEY(classes_room_number, classes_time) REFERENCES counts(counts_room_number, counts_time)
 );
 
 drop table if exists rooms;
-create table rooms (
-  rooms_rooms_number string primary key not null,
+CREATE TABLE rooms (
+  rooms_rooms_number string not null,
   rooms_build string not null,
   rooms_campus string not null,
-  rooms_capacity integer
+  rooms_capacity integer,
+  PRIMARY KEY (rooms_rooms_number),
+  FOREIGN KEY (rooms_rooms_number) REFERENCES classes(classes_room_number)
 );
 
 drop table if exists counts;
-create table counts (
-  counts_room_number string primary key not null,
-  counts_time datetime primary key,
+CREATE TABLE counts (
+  counts_room_number string not null,
+  counts_time datetime,
   counts_module_code string,
   counts_associated integer,
   counts_authenticated integer,
   counts_truth_percent string,
-  counts_truth integer
+  counts_truth integer,
+  PRIMARY KEY (counts_room_number, counts_time)
 );
