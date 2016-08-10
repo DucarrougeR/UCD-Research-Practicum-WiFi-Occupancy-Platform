4917440a95cfef65c048478d63120f2f0cc3087f
Mark <markgyr@gmail.com>
2016-08-10 14:25:58 +0100

    Moved files in /mod_sensors

--- calibrate_sensors.py
+++ calibrate_sensors.py
@@ -1,3 +1,3 @@
 from app.mod_sensors import *
 
-calibration.calibrate_rssi_baseline("app/mod_sensors/rssi/baseline_readings/test.csv", "B002")
+calibration.calibrate_rssi_baseline("app/mod_sensors/data/rssi/baseline_readings/test.csv", "B002")
