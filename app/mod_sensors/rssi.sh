bac2dac3d5d02d9b9aa6d9c3e19dc62b185b22f2
Mark <markgyr@gmail.com>
2016-08-13 12:14:25 +0100

    Bash script for gathering RSSI data

---
+++ app/mod_sensors/rssi.sh
@@ -0,0 +1,7 @@
+rdir="data/"; now=$(date "+%d-%m-%Y-%H:%M:%S"); format=".csv"; type="rssi"; rfilename=$rdir$type$now$format 
+printf "date,month,year,time,rssi" >> $rfilename 
+while true; do
+printf "\n$(date "+%d,%m,%Y,%H:%M:%S"),$( iwconfig wlan0 \
+    | grep "Signal level" | cut -d '-' -f 2 | cut -d ' ' -f 1)" >> $rfilename; \
+    sleep 30; 
+done
