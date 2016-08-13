af155cc92a1eb7daf81435a25c6e9549a28b28b2
Mark <markgyr@gmail.com>
2016-08-13 14:26:00 +0100

    sensors.sh now takes room number argument

--- app/mod_sensors/calibration.py
+++ app/mod_sensors/calibration.py
@@ -1,25 +1,20 @@
 import pandas as pd
 from ..mod_db import *
 
-def calibrate_rssi_baseline(csv, room):
+def calibrate_baselines(csv, room):
     """
-    Takes a CSV file of RSSI readings from an empty room, calculates a
-    baseline and writes it to the database. 
+    Takes a CSV file of sensor readings from an empty room, calculates 
+    baselines and writes them to the database. 
     """
 
     # Reads the input CSV file. 
     df = pd.read_csv(csv)
 
     # Calculates the baseline as the mode of RSSI values in the infile. 
-    baseline = df["rssi"].mode()[0]
-
-    print(baseline)
+    baseline_audio = df["maxamp"].mode()[0]
+    baseline_rssi = df["rssi"].mode()[0]
 
     # Writes the calculated baseline to the database for that room. 
-    query = Rooms.update(rssi_baseline=baseline).where(Rooms.room_number == room)
+    query = Rooms.update(room_rssi_baseline=baseline_rssi, room_audio_baseline=baseline_audio).where(Rooms.room_number == room)
     query.execute()
 
-def calibrate_audio_baseline():
-    """
-
-    """
--- app/mod_sensors/sensors.sh
+++ app/mod_sensors/sensors.sh
@@ -1,8 +1,11 @@
-dir="data/"; now=$(date "+%d-%m-%Y-%H:%M:%S"); format=".csv"; filename=$dir$now$format 
-printf "date,month,year,time,maxamp,rssi"
+# Writes a CSV file containing RSSI readings from the wlan0 interface
+# and the maximum amplitude in a recorded wav file in 30-second intervals. 
+# Takes 1 argument, which should be a room number, e.g., B002
+dir="data/"; now=$(date "+%d-%m-%Y-%H:%M:%S"); format=".csv"; filename=$dir$1$now$format 
+printf "room,date,month,year,time,maxamp,rssi"
 while true; do
     arecord -d 30 temp.wav 
-    printf "\n$(date "+%d,%m,%Y,%H:%M:%S"),$(sox temp.wav -n stat 2>&1 | grep "Maximum amplitude" | cut -d ':' -f 2 | tr -d '[[:space:]]')" >> $filename; 
+    printf "\n$1,$(date "+%d,%m,%Y,%H:%M:%S"),$(sox temp.wav -n stat 2>&1 | grep "Maximum amplitude" | cut -d ':' -f 2 | tr -d '[[:space:]]')" >> $filename; 
     printf "$( iwconfig wlan0 | grep "Signal level" | cut -d '-' -f 2 | cut -d ' ' -f 1)" >> $filename; 
 done
     
