83d7cc2a3a0e947b26efecd9d4081a59b6db9ee2
Mark <markgyr@gmail.com>
2016-08-15 15:21:21 +0100

    Modified face detection

--- app/mod_sensors/video.py
+++ app/mod_sensors/video.py
@@ -11,14 +11,16 @@ def monitor(room):
     cmd = 'printf "room,date,month,year,time,face" >> "{0}"'.format(filename)
     os.system(cmd)
 
+    # Specifies and instantiates the classifier used.
+    cascpath = "haarcascade_frontalface_default.xml"
+    facecascade = cv2.CascadeClassifier(cascpath)
+
     while True: 
+        face_detected = False
+
         # For 30 seconds:
         loop_time = time.time() + 30
         while time.time() < loop_time:
-            # Specifies and instantiates the classifier used.
-            cascPath = "haarcascade_frontalface_default.xml"
-            faceCascade = cv2.CascadeClassifier(cascPath)
-            
             # Captures a video frame by frame. 
             video_capture = cv2.VideoCapture(0)
             ret, frame = video_capture.read()
@@ -27,28 +29,24 @@ def monitor(room):
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # Detects faces using the classifier. 
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.2,
-                minNeighbors=10,
-                minSize=(30, 30),
-                flags=cv2.CASCADE_SCALE_IMAGE
+            faces = facecascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10, 
+                    minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE
             )
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            
+            # Records if any faces were detected. 
+            if numpy.any(faces):
+                face_detected = True
+
             # Sleeps for 2 seconds so there aren't too many frames to process. 
-            time.sleep(2)
+            time.sleep(2)            
 
         # Writes to the outfile whether a face was detected or not. 
-        if numpy.any(faces):
+        if face_detected:
             os.system('printf "\n{0},$(date "+%d,%m,%Y,%H:%M:%S"),1", >> "{1}"'.format(room, filename))
         else:
             os.system('printf "\n{0},$(date "+%d,%m,%Y,%H:%M:%S"),0", >> "{1}"'.format(room, filename))
-            
+
         # Releases the capture. 
         video_capture.release()
-        cv2.destroyAllWindows()
 
 monitor("B002")
