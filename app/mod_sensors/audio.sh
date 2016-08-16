331ef04de88ed8d29a963e7170d7df1d92e7eb1c
Mark <markgyr@gmail.com>
2016-08-16 16:32:53 +0100

    Raspberry Pi setup file. Fixed CRLF in audio.sh

--- app/mod_sensors/audio.sh
+++ app/mod_sensors/audio.sh
@@ -1,6 +1,6 @@
-adir="data/"; now=$(date "+%d-%m-%Y-%H-%M-%S"); format=".csv"; type="audio"; afilename=$adir$type$now$format 
-printf "date,month,year,time,maxamp" >> $afilename 
+adir="data/"; now=$(date "+%d-%m-%Y-%H-%M-%S"); format=".csv"; type="audio"; filename=$adir$type$now$format
+printf "date,month,year,time,maxamp" >> $filename 
 while true; do 
     arecord -d 30 temp.wav 
-    printf "\n$(date "+%d,%m,%Y,%H:%M:%S"),$(sox temp.wav -n stat 2>&1 | grep "Maximum amplitude" | cut -d ':' -f 2 | tr -d '[[:space:]]')" >> $afilename; 
+    printf "\n$(date "+%d,%m,%Y,%H:%M:%S"),$(sox temp.wav -n stat 2>&1 | grep "Maximum amplitude" | cut -d ':' -f 2 | tr -d '[[:space:]]')" >> $filename; 
 done
